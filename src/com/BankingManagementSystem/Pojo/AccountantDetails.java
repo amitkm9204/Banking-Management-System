c036a62821caac754c4e437a896daff33e7ef676
AMIT KUMAR <AMIT KUMAR>
2017-01-27 22:56:01 +0530

    adding accountant details

--- src/com/BankingManagementSystem/Pojo/AccountantDetails.java
+++ src/com/BankingManagementSystem/Pojo/AccountantDetails.java
@@ -2,4 +2,26 @@ package com.BankingManagementSystem.Pojo;
 
 public class AccountantDetails {
 
+	private String AccountantName;
+	private String AccountantId;
+	private String AccountantPassword;
+	public String getAccountantName() {
+		return AccountantName;
+	}
+	public void setAccountantName(String accountantName) {
+		AccountantName = accountantName;
+	}
+	public String getAccountantId() {
+		return AccountantId;
+	}
+	public void setAccountantId(String accountantId) {
+		AccountantId = accountantId;
+	}
+	public String getAccountantPassword() {
+		return AccountantPassword;
+	}
+	public void setAccountantPassword(String accountantPassword) {
+		AccountantPassword = accountantPassword;
+	}
+	
 }
