4c2071d5534b1a6ba2910d0dcbaebb0e3945a67e
AMIT KUMAR <AMIT KUMAR>
2017-01-27 22:51:27 +0530

    adding the filehandler for accountant

--- src/com/BankingManagementSystem/FileHandling/AccountantDetailsFile.java
+++ src/com/BankingManagementSystem/FileHandling/AccountantDetailsFile.java
@@ -1,5 +1,45 @@
 package com.BankingManagementSystem.FileHandling;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+import com.BankingManagementSystem.Pojo.AccountantDetails;
+
 public class AccountantDetailsFile {
+	public static ArrayList<AccountantDetails> readDataFromFile()
+	{
+		ArrayList<AccountantDetails> list4;
+		try
+		{
+		   FileInputStream fin=new FileInputStream("AccountantDetails.dat");
+		   ObjectInputStream oin=new ObjectInputStream(fin);
+		   list4=(ArrayList<AccountantDetails>)oin.readObject();
+		   
+		   oin.close();
+		   fin.close();
+		}catch(Exception e)
+		 {
+			 list4=new ArrayList<AccountantDetails>();
+		 }
+		 
+		return list4;
+		
+	}
+	public static void writeDatatoFile(ArrayList<AccountantDetails> wlist)
+	{
+		try	
+		 {
+		       FileOutputStream fout=new FileOutputStream("AccountantDetails.dat");
+		       ObjectOutputStream oout=new ObjectOutputStream(fout);
+		       oout.writeObject(wlist);
+		       
+		       oout.close();
+		}catch(Exception e){System.out.println(e);}
+	}
+
+
 
 }
